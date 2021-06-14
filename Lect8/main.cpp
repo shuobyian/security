8a93cf9edc77f0c1f2dfb0eb185f2d040cd9a079
shuobyian <chickby@hanmail.net>
2021-06-14 19:29:05 +0900

    DES
    DES 정보보호론

---
+++ Lect8/main.cpp
@@ -0,0 +1,24 @@
+#include "global.h"
+#include <time.h>
+#include <math.h>
+#include <random>
+
+void display_sentence(char *cipher){
+   printf("cipher:\n");
+   for(int i=0;i<64;i++)
+      printf("%d ", cipher[i]);
+   printf("\n");
+}
+int main(){
+   char keys[17][48];
+   char cipher[64]; // cipher text
+
+   srand((unsigned int)time(NULL));
+   for (int i = 0; i < 64; i++)
+	   K[i] = rand() % 2;
+   construct_key_schedule(K,keys); // construct key schedule
+
+   //construct_key_schedule(K, keys); // K is original 64bit key
+   des_encrypt(M, keys, cipher);
+   display_sentence(cipher);
+}
